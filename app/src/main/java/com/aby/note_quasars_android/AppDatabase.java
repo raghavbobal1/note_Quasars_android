e4cc89236339161495449ead13e9643c9e84dc58
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-07-12 11:21:49 -0400

    Added recycler view, adapter and database operations managing class

---
+++ app/src/main/java/com/aby/note_quasars_android/AppDatabase.java
@@ -0,0 +1,28 @@
+package com.aby.note_quasars_android;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Note.class},version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase INSTANCE;
+
+    public abstract NoteDao noteDao();
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE =
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "database-name")
+                            .build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+}
